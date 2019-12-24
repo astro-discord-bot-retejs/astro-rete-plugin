032b93ad8c62fdddbe4bd82eba93a77cd2cda0df
Jonas <contact@atalantus.de>
2019-12-24 15:03:38 +0100

    Removed boilerplate

--- projects/angular-render-plugin/src/public-api.ts
+++ projects/angular-render-plugin/src/public-api.ts
@@ -1,7 +1,3 @@
 /*
  * Public API Surface of angular-render-plugin
  */
-
-export * from './lib/angular-render-plugin.service';
-export * from './lib/angular-render-plugin.component';
-export * from './lib/angular-render-plugin.module';
--- projects/astro-node-editor/src/app/app.component.spec.ts
+++ projects/astro-node-editor/src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'astro-node-editor'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('astro-node-editor');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('astro-node-editor app is running!');
-  });
 });
