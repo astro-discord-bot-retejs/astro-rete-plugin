093950a1d38eada062b58bd91c166548724481ac
Jonas <contact@atalantus.de>
2020-02-01 18:39:28 +0100

    Fixed NodeComponent test

--- projects/angular-render-plugin/src/lib/components/node/node.component.spec.ts
+++ projects/angular-render-plugin/src/lib/components/node/node.component.spec.ts
@@ -1,26 +1,28 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { NodeComponent } from './node.component';
+import {NodeComponent} from './node.component';
 import {KebabPipe} from '../../pipes/kebab.pipe';
 import {SocketDirective} from '../../directives/socket.directive';
 import {SocketComponent} from '../socket/socket.component';
 import {ControlDirective} from '../../directives/control.directive';
-import {NodeService} from '../../services/node.service';
+import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {Node} from 'rete';
 
 describe('NodeComponent', () => {
   let component: NodeComponent;
-  let fixture: ComponentFixture<NodeComponent>;
+  let fixture: ComponentFixture<TestNodeComponentWrapper>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NodeComponent, KebabPipe, SocketDirective, SocketComponent, ControlDirective ]
+      declarations: [TestNodeComponentWrapper, NodeComponent, KebabPipe, SocketDirective, SocketComponent, ControlDirective],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NodeComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestNodeComponentWrapper);
+    component = fixture.debugElement.children[0].componentInstance;
     fixture.detectChanges();
   });
 
@@ -28,3 +30,11 @@ describe('NodeComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+@Component({
+  template: '<node-component [node]="node"></node-component>'
+})
+// tslint:disable-next-line:component-class-suffix
+class TestNodeComponentWrapper {
+  node = new Node('test');
+}
